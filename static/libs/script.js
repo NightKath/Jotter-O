19e93506b3179d31cf4eda5d22146754aea54b38
Katherine Nicole Bayan <146793275+NightKath@users.noreply.github.com>
2024-12-05 16:42:09 +0800

    Delete static directory

--- static/libs/script.js
+++
@@ -1,5 +0,0 @@
-// Add the following JavaScript code to your script.js file
-window.addEventListener('scroll', function() {
-    document.documentElement.style.setProperty('--scrollTop', window.scrollY + 'px');
-});
-
